97686e3bc95ca354bd25cc492f4dcd5d1d885e7d
Gabriel Cerqueira <gabrielcmo2018@gmail.com>
2021-04-05 20:36:37 -0300

    /*

---
+++ precache-manifest.bcb93801bc2ea54e4b9609ddfdb81485.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91cf0df27dc0e4c6bab046520d3ccf79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0216cba99be4987ed66",
+    "url": "/static/css/2.f9825ca1.chunk.css"
+  },
+  {
+    "revision": "f0216cba99be4987ed66",
+    "url": "/static/js/2.056abfa4.chunk.js"
+  },
+  {
+    "revision": "7b515d4e613a24d756d8fcf9d0018e75",
+    "url": "/static/js/2.056abfa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d58e325b2b1a0b264bc9",
+    "url": "/static/js/main.7ac33843.chunk.js"
+  },
+  {
+    "revision": "38d9f8f32915c931ff75",
+    "url": "/static/js/runtime-main.45aea16f.js"
+  },
+  {
+    "revision": "a7508486260bd88516508a9aece7e2dd",
+    "url": "/static/media/checkEdit.a7508486.png"
+  },
+  {
+    "revision": "0f03092c3bd57d56e232fe82ebe84c96",
+    "url": "/static/media/faceuser.0f03092c.jpg"
+  },
+  {
+    "revision": "de235bf7717e6d912eca0407efcc012b",
+    "url": "/static/media/false.de235bf7.png"
+  },
+  {
+    "revision": "9eb3ca3d21560c7d371cc62db92736cf",
+    "url": "/static/media/icoLoading.9eb3ca3d.gif"
+  },
+  {
+    "revision": "4e542119f051e0ef425fd7afc9cb17a0",
+    "url": "/static/media/icoconfig.4e542119.png"
+  },
+  {
+    "revision": "b26108e0fe047739f4081e3e39a7994b",
+    "url": "/static/media/img_388620.b26108e0.png"
+  },
+  {
+    "revision": "6997039d20d15ebe04e53f56e730b22e",
+    "url": "/static/media/letter-ico.6997039d.png"
+  },
+  {
+    "revision": "e9e139ce0f161d0ea93e7e4d289efe18",
+    "url": "/static/media/true.e9e139ce.png"
+  }
+]);
